5ca2a5a21a9c4da9bf54fb94e2596d3b4c775720
Maksymilian Górka <44338133+gorkaMaksymilian@users.noreply.github.com>
2019-05-07 21:17:42 +0000

    Create RMMELATER.java

---
+++ RMMELATER.java
@@ -0,0 +1,146 @@
+
+Szukaj
+
+
+
+
+
+1
+
+
+ 
+9:15 / 56:20
+Gra o Tron #4: Zmarnowane osiem lat? (dyskusja po odcinku)
+19 187 wyświetleń
+
+912
+
+46
+
+UDOSTĘPNIJ
+
+ZAPISZ
+
+
+Napisy Końcowe
+Opublikowany 6 maj 2019
+
+Napisy Końcowe to kanał dyskusyjny tworzony przez Łukasza Stelmacha, Oskara Rogowskiego, Radosława Pisulę, Martę Najman, Adama Antolskiego i przyjaciół. Znajdziecie tutaj komentarze i dyskusje na temat doniesień ze świata filmu oraz popkultury.
+
+Słuchaj na Spotify: https://open.spotify.com/show/0B4DqgC...
+Słuchaj na iTunes: https://itunes.apple.com/pl/podcast/n...
+RSS: https://www.podsluchane.link/nk-rss
+
+Facebook: https://www.facebook.com/nkoncowe/
+
+Łukasz Stelmach https://www.youtube.com/user/lstelmach89
+Oskar Rogowski https://www.youtube.com/channel/UCyee...
+Radosław Pisula https://pl-pl.facebook.com/fullfronta...
+Marta Najman https://www.facebook.com/MartaOceanso...
+
+Zapytania biznesowe: lstelmach89@gmail.com
+Następny
+AUTOODTWARZANIE
+
+13:28
+Największe porażki finansowe wśród filmów
+TvFilmy
+17 tys. wyświetleń
+Nowy
+
+
+27:44
+Who's in ACTUAL CONTROL?! Nerf Dr. Boom? | Hagatha Control Shaman | Rise of Shadows | Hearthstone
+Disguised Toast
+Polecane dla Ciebie
+Nowy
+
+
+3:35
+grimmy - brawurowo i pusto. raczej bez światła.
+HASHA DRAFT
+Polecane dla Ciebie
+
+
+2:16:15
+Kapitan Marvel – dyskusja spoilerowa. Znaczenie filmu, zaskoczenia, scena po napisach
+Napisy Końcowe
+42 tys. wyświetleń
+
+
+29:41
+Smarki Smark - Najebawszy EP (Moda na EPke) (Całość)
+jacek
+Polecane dla Ciebie
+
+GIMPER szczerze o: YouTube, przeszłości i Lekko Nie Będzie
+Imponderabilia
+Polecane dla Ciebie
+
+Mata - Biblioteka Trap ◾️ SBM Starter ◾️
+SBM Starter
+Polecane dla Ciebie
+
+Ch**a się znacie. Znam swoją wartość - Człowiek WARGA | Imponderabilia #44
+Imponderabilia
+11 tys. wyświetleń
+Nowy
+
+ERKING - PRZYSTANKI NA LOTNISKA
+Blackout TV
+Polecane dla Ciebie
+
+Emes Milligan - FIGHT CLUB
+YUSH.
+Polecane dla Ciebie
+Nowy
+
+Gra o Tron (Sezon1) Przypomnienie - Marzec z GOTem ★RetroRecenzje
+Ponarzekajmy o Filmach
+Polecane dla Ciebie
+
+Czarna śmierć. Jak umarła 1/3 mieszkańców Europy. Historia Bez Cenzury
+Historia bez cenzury
+Polecane dla Ciebie
+Nowy
+
+GTA 6 Vice City na PlayStation 5 i Xbox Anaconda
+Remigiusz Maciaszek
+Polecane dla Ciebie
+Nowy
+
+Quiz Pod Napięciem – odc. 16 (WESZŁO!) | ETOTO TV
+ETOTO TV
+111 tys. wyświetleń
+Nowy
+
+Avengers: Koniec Gry – dyskusja spoilerowa. Omówienie wątków, postaci i motywów
+Napisy Końcowe
+64 tys. wyświetleń
+Nowy
+
+Gra o Tron - Sezon 8 Odcinek 4 ★SzybkieRecenzje
+Ponarzekajmy o Filmach
+53 tys. wyświetleń
+Nowy
+
+Epilog po Endgame. Pełny zwiastun Spider-Man: Daleko od domu
+Napisy Końcowe
+26 tys. wyświetleń
+Nowy
+
+Grzegorz Dolniak stand-up - FLASHBACK (cały program)
+Grzegorz Dolniak
+1 mln wyświetleń
+
+Endgame Najlepsze Sceny i Teorie (Co Dalej?)
+Herosół
+43 tys. wyświetleń
+Nowy
+
+Gra o Tron: omawiamy odcinek 4 sezonu 8. NA SZYBKO + SPOILERY
+jakbyniepaczec
+222 tys. wyświetleń
+Nowy
+
+
